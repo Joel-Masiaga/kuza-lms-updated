36171b2217de8dcee067d2e045fdd7abb8fc95b0
Joel Masiaga <masiagajoel001@gmail.com>
2025-11-07 13:41:53 +0300

    New commit

--- lms/lms/settings.py
+++ lms/lms/settings.py
@@ -185,7 +185,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET')
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+    'RESOURCE_TYPE': 'auto',
 }
 
 
